375cc965c129fc3573e5aae92cb325fe94e1fe9a
gogocj <gogocj132438@sina.com>
2021-06-04 18:43:41 +0800

    first commit

---
+++ .vuepress/nav.js
@@ -0,0 +1,21 @@
+module.exports = [
+    {
+        text: '博客笔记', link: '/guide/'
+    },
+    {
+        text: '面试手记',link:"/baodian/interview/"
+		// items: [
+        //     {text: '初级开发篇', link: '/baodian/zero/'},
+        //     {text: '中高进阶篇', link: '/baodian/high/'},
+        // ]
+    },
+    {
+        text: '大杰树洞',link:"/baodian/talk/"
+    },
+   
+    {
+        text: '关于我的', link: 'https://space.bilibili.com/145679856',
+
+    },
+   
+]
